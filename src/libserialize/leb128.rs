f5d858b203bcecacbd6ac84e46c1f19842e93e26
Jonas Schievink <jonasschievink@gmail.com>
2020-03-21 14:40:31 +0100

    wip

--- src/libserialize/leb128.rs
+++ src/libserialize/leb128.rs
@@ -1,25 +1,45 @@
+use std::intrinsics::unlikely;
+
 macro_rules! impl_write_unsigned_leb128 {
     ($fn_name:ident, $int_ty:ident) => {
         #[inline]
         pub fn $fn_name(out: &mut Vec<u8>, mut value: $int_ty) {
-            loop {
-                if value < 0x80 {
-                    out.push(value as u8);
-                    break;
-                } else {
-                    out.push(((value & 0x7f) | 0x80) as u8);
-                    value >>= 7;
+            if value < u32::max_value() as $int_ty {
+                write_u32_leb128(out, value as u32);
+            } else {
+                loop {
+                    if value < 0x80 {
+                        out.push(value as u8);
+                        break;
+                    } else {
+                        out.push(((value & 0x7f) | 0x80) as u8);
+                        value >>= 7;
+                    }
                 }
             }
         }
     };
 }
 
-impl_write_unsigned_leb128!(write_u16_leb128, u16);
-impl_write_unsigned_leb128!(write_u32_leb128, u32);
 impl_write_unsigned_leb128!(write_u64_leb128, u64);
 impl_write_unsigned_leb128!(write_u128_leb128, u128);
 impl_write_unsigned_leb128!(write_usize_leb128, usize);
+
+#[inline]
+pub fn write_u16_leb128(out: &mut Vec<u8>, value: u16) {
+    write_u32_leb128(out, value.into());
+}
+
+#[inline]
+pub fn write_u32_leb128(out: &mut Vec<u8>, value: u32) {
+    let hi = 0x8080_8080_8080_8080;
+    let split = unsafe { std::arch::x86_64::_pdep_u64(value as u64, !hi) };
+    let leading0s = (split | 1).leading_zeros();
+    let tags = (!0 >> leading0s) & hi;
+    let leb128 = split | tags;
+    let bytes = 8 - (leading0s / 8);
+    out.extend_from_slice(&leb128.to_le_bytes()[..bytes as usize]);
+}
 
 macro_rules! impl_read_unsigned_leb128 {
     ($fn_name:ident, $int_ty:ident) => {
@@ -43,9 +63,39 @@ macro_rules! impl_read_unsigned_leb128 {
     };
 }
 
+fn read_slow(slice: &[u8]) -> (u128, usize) {
+    let mut result = 0;
+    let mut shift = 0;
+    let mut position = 0;
+    loop {
+        let byte = slice[position];
+        position += 1;
+        if (byte & 0x80) == 0 {
+            result |= (byte as u128) << shift;
+            return (result, position);
+        } else {
+            result |= ((byte & 0x7F) as u128) << shift;
+        }
+        shift += 7;
+    }
+}
+
+#[inline]
+pub fn read_u64_leb128(slice: &[u8]) -> (u64, usize) {
+    if unlikely(slice.len() < 8) {
+        let (v, count) = read_slow(slice);
+        (v as u64, count)
+    } else {
+        // Up to 8 encoded bytes can be decoded at once via SIMD.
+        let mut arr = [0u8; 8];
+        arr.copy_from_slice(&slice[..8]);
+
+        todo!()
+    }
+}
+
 impl_read_unsigned_leb128!(read_u16_leb128, u16);
 impl_read_unsigned_leb128!(read_u32_leb128, u32);
-impl_read_unsigned_leb128!(read_u64_leb128, u64);
 impl_read_unsigned_leb128!(read_u128_leb128, u128);
 impl_read_unsigned_leb128!(read_usize_leb128, usize);
 
--- src/libserialize/lib.rs
+++ src/libserialize/lib.rs
@@ -14,6 +14,7 @@ Core encoding and decoding interfaces.
 #![feature(never_type)]
 #![feature(nll)]
 #![feature(associated_type_bounds)]
+#![feature(core_intrinsics)]
 #![cfg_attr(test, feature(test))]
 #![allow(rustc::internal)]
 
